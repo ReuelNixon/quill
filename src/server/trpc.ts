a43a963526e84d05fc436c3397c16cb34d12ffa8
Reuel Nixon <reuelnixon@gmail.com>
2023-10-14 20:13:49 +0530

    Added a protected procedure

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,21 @@
-import { initTRPC } from '@trpc/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { TRPCError, initTRPC } from '@trpc/server';
 
 const t = initTRPC.create();
+const middleware = t.middleware;
 
+const isAuthenticated = middleware(async (opts) => {
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
+
+	if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+	return opts.next({
+		ctx: {
+			userId: user.id,
+			user: user,
+		},
+	});
+});
 export const router = t.router;
 export const procedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuthenticated);
