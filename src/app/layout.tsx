3eaf4c5814ce5f88081007745102d512e564a0ef
Reuel Nixon <reuelnixon@gmail.com>
2023-10-14 21:28:30 +0530

    Added css for react-loading-skeleton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import 'react-loading-skeleton/dist/skeleton.css';
 import Provider from './_trpc/Provider';
 import './globals.css';
 
