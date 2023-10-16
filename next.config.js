7c9638f05cbfb04f1b55ffd593007942455c776e
Reuel Nixon <reuelnixon@gmail.com>
2023-10-16 19:35:49 +0530

    Fiishing up the incomplete navbar

--- next.config.js
+++ next.config.js
@@ -7,7 +7,24 @@ const nextConfig = {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
         return config
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/sign-in',
+                destination: '/api/auth/login',
+                permanent: true,
+            },
+            {
+                source: '/sign-up',
+                destination: '/api/auth/register',
+                permanent: true,
+            },
+        ]
+    },
+    images: {
+        domains: ['localhost', 'lh3.googleusercontent.com'],
+    },
 }
 
 
