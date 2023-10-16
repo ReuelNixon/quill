40b6c641dc6b15d1405e3ad7b0d743d41f52699a
Reuel Nixon <reuelnixon@gmail.com>
2023-10-16 18:08:05 +0530

    Added the chat feature

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,30 +2,104 @@ import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
+import { getPineconeClient } from '@/lib/pinecone';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+
 const f = createUploadthing();
+
+const middleware = async () => {
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
+
+	if (!user || !user.id) throw new Error('Unauthorized');
+
+	return { userId: user.id };
+};
+
+const onUploadComplete = async ({
+	metadata,
+	file,
+}: {
+	metadata: Awaited<ReturnType<typeof middleware>>;
+	file: {
+		key: string;
+		name: string;
+		url: string;
+	};
+}) => {
+	const isFileExist = await db.file.findFirst({
+		where: {
+			key: file.key,
+		},
+	});
+
+	if (isFileExist) return;
+
+	const createdFile = await db.file.create({
+		data: {
+			key: file.key,
+			name: file.name,
+			userId: metadata.userId,
+			url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+			uploadStatus: 'PROCESSING',
+		},
+	});
+
+	try {
+		const response = await fetch(
+			`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+		);
+
+		const blob = await response.blob();
+
+		const loader = new PDFLoader(blob);
+
+		const pageLevelDocs = await loader.load();
+
+		const pagesAmt = pageLevelDocs.length;
+
+		// vectorize and index entire document
+		const pinecone = await getPineconeClient();
+		const pineconeIndex = await pinecone.Index('quill');
+
+		const embeddings = new OpenAIEmbeddings({
+			openAIApiKey: process.env.OPENAI_API_KEY,
+		});
+
+		console.log(createdFile.id);
+
+		await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+			pineconeIndex,
+			namespace: createdFile.id,
+		});
+
+		await db.file.update({
+			data: {
+				uploadStatus: 'SUCCESS',
+			},
+			where: {
+				id: createdFile.id,
+			},
+		});
+	} catch (err) {
+		console.error(err);
+		await db.file.update({
+			data: {
+				uploadStatus: 'FAILED',
+			},
+			where: {
+				id: createdFile.id,
+			},
+		});
+	}
+};
 
 export const ourFileRouter = {
 	pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
-		.middleware(async ({ req }) => {
-			const { getUser } = getKindeServerSession();
-			const user = getUser();
-
-			if (!user.id || !user.email) throw new Error('Unauthorized');
-			return {
-				userId: user.id,
-			};
-		})
-		.onUploadComplete(async ({ metadata, file }) => {
-			const createdFile = await db.file.create({
-				data: {
-					name: file.name,
-					key: file.key,
-					userId: metadata.userId,
-					url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-					uploadStatus: 'PROCESSING',
-				},
-			});
-		}),
+		.middleware(middleware)
+		.onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
